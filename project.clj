97d3051cee5eb7a8b7ae6d18140ab9981f959dd1
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-03 22:05:48 -0700

    add ring-json-params dependency

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
      [ring/ring-jetty-adapter "0.2.5"]
      [compojure "0.4.0"]
      [hiccup "0.2.6"]
-     [clj-json "0.3.0-SNAPSHOT"]]
+     [clj-json "0.3.0-SNAPSHOT"]
+     [ring-json-params "0.1.0-SNAPSHOT"]]
   :dev-dependencies
     [[lein-run "1.0.0-SNAPSHOT"]])
