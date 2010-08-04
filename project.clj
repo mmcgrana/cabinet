913d5bdf413f5550eca7feb6187e3a2a4d1c266e
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-04 10:41:12 -0700

    trim project more

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :dependencies
     [[org.clojure/clojure "1.2.0-RC1"]
      [org.clojure/clojure-contrib "1.2.0-RC1"]
-     [ring/ring-core "0.2.5"]
-     [ring/ring-devel "0.2.5"]
      [ring/ring-jetty-adapter "0.2.5"]
      [ring-json-params "0.1.0"]
      [compojure "0.4.0"]
