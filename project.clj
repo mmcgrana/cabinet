4201b35d2367930ab3b444e43d8395088e093aeb
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-04 08:36:28 -0700

    tweak project file

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
 (defproject cabinent "0.0.1"
-  :description "JSON datastore interface."
+  :description "REST datastore interface."
   :dependencies
     [[org.clojure/clojure "1.2.0-RC1"]
      [org.clojure/clojure-contrib "1.2.0-RC1"]
      [ring/ring-core "0.2.5"]
      [ring/ring-devel "0.2.5"]
      [ring/ring-jetty-adapter "0.2.5"]
+     [ring-json-params "0.1.0"]
      [compojure "0.4.0"]
-     [hiccup "0.2.6"]
-     [clj-json "0.3.0-SNAPSHOT"]
-     [ring-json-params "0.1.0-SNAPSHOT"]]
+     [clj-json "0.2.0"]]
   :dev-dependencies
     [[lein-run "1.0.0-SNAPSHOT"]])
