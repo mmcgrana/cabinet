ab59260fe12cecb708ca5ba9723e6b025b1a60d8
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-03 22:59:34 -0700

    reload, failsafe, json parsing error handling

--- src/cabinet/elem.clj
+++ src/cabinet/elem.clj
@@ -1,4 +1,5 @@
 (ns cabinet.elem
+  (:use clojure.contrib.condition)
   (:refer-clojure :exclude (get delete)))
 
 (def elems (atom {}))
@@ -7,12 +8,17 @@
   @elems)
 
 (defn get [id]
-  (@elems id))
+  (or (@elems id)
+      (raise :message "elem not found"
+             :status 404)))
 
 (defn put [id attrs]
-  (let [new-attrs (merge (get id) attrs)]
-    (swap! elems assoc id new-attrs)
-    new-attrs))
+  (if (empty? attrs)
+    (raise :message "attrs are empty"
+           :status 400)
+    (let [new-attrs (merge (get id) attrs)]
+      (swap! elems assoc id new-attrs)
+      new-attrs)))
 
 (defn delete [id]
   (let [old-attrs (get id)]
--- src/cabinet/web.clj
+++ src/cabinet/web.clj
@@ -1,13 +1,38 @@
 (ns cabinet.web
   (:use compojure.core)
+  (:use ring.middleware.reload)
   (:use ring.middleware.json-params)
   (:require [clj-json.core :as json])
-  (:require [cabinet.elem :as elem]))
+  (:require [cabinet.elem :as elem])
+  (:import org.codehaus.jackson.JsonParseException)
+  (:import clojure.contrib.condition.Condition))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/json"}
    :body (json/generate-string data)})
+
+(def error-info
+  {:attributes-invalid [400 "attributes invalid"]
+   :elem-not-found     [404 "elem not found"]})
+
+(defn wrap-error-handling [handler]
+  (fn [req]
+    (try
+      (or (handler req)
+          (json-response {"error" "resource not found"} 404))
+      (catch JsonParseException e
+        (json-response {"error" "malformed json"} 400))
+      (catch Condition e
+        (let [{:keys [message status]} (meta e)]
+          (json-response {"error" message} status))))))
+
+(defn wrap-failsafe [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Exception e
+        (json-response {"error" "internal server error"} 500)))))
 
 (defroutes handler
   (GET "/elems" []
@@ -23,5 +48,8 @@
     (json-response (elem/delete id))))
 
 (def app
-  (-> handler
-    (wrap-json-params)))
+  (-> #'handler
+    (wrap-json-params)
+    (wrap-error-handling)
+    (wrap-failsafe)
+    (wrap-reload ['cabinet.web 'cabinet.elem])))
